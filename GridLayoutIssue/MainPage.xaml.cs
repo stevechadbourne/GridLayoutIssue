93cd3ba21aab71eaf139fb13c4be54f6544edede
Steve Chadbourne <steve.chadbourne@univerus.com>
2024-04-11 15:11:13 +1200

    Add project files.

---
+++ GridLayoutIssue/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace GridLayoutIssue
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_OnTapped1(object sender, TappedEventArgs e)
+        {
+            BottomBox1.IsVisible = !BottomBox1.IsVisible;
+        }
+
+        private void TapGestureRecognizer_OnTapped2(object sender, TappedEventArgs e)
+        {
+            BottomBox2.IsVisible = !BottomBox2.IsVisible;
+        }
+    }
+
+}
